89b30dd5653756682e6249a6fc0fe9afd4764fbb
udayrana428 <84346191+udayrana428@users.noreply.github.com>
2024-05-27 18:53:38 +0000

    completed variables and some part of datatypes

---
+++ 01_variables.js
@@ -0,0 +1,9 @@
+const accountId =12345
+let accountEmail = "udayrana428@gmail.com"
+var accoundPassword = "12345"
+
+accountCity="jaipur"
+console.table([accountId,accountEmail, accoundPassword,accountCity])
+console.log(accountId)
+
+// Prefer not to use var variable because of issue in block scope and functional scope.
---
+++ 02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(2+3) we are using nodejs not browser that's why alert will not work here
+
+// Code Readibility
+
+console.log("uday"); console.log("rana") // This is not a correct approach to write code this is not readible clearly so we don't use this type of approach
+
+console.log(3+
+    3, "uday"
+) // This is also not a good approach
+
+// Note: Alaways try to write the code properly so that is should be readble and also easily understable by other programmer.
+
+
+// Datatypes
+
+let name = "uday"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint (specially used in stocks where big number are used)
+// string => "uday"
+// null => standalone value
+// undefi
